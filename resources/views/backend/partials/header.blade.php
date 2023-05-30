c9a152e605abd2ccfe3b27ba09769889174b8849
thoyiburrohman <thoyibur@gmail.com>
2023-05-30 11:46:03 +0700

    add & edit logo

--- resources/views/backend/partials/header.blade.php
+++ resources/views/backend/partials/header.blade.php
@@ -6,19 +6,19 @@
             <div class="navbar-brand-box">
                 <a href="index.html" class="logo logo-dark">
                     <span class="logo-sm">
-                        <img src="{{ asset('images/logo-sm.png') }}" alt="" height="22">
+                        <img src="{{ asset('images/logo-sm-new.png') }}" alt="" height="22">
                     </span>
                     <span class="logo-lg">
-                        <img src="{{ asset('images/logo-dark.png') }}" alt="" height="20">
+                        <img src="{{ asset('images/logo-sm-new.png') }}" alt="" height="20">
                     </span>
                 </a>
 
                 <a href="index.html" class="logo logo-light">
                     <span class="logo-sm">
-                        <img src="{{ asset('images/logo-sm.png') }}" alt="" height="22">
+                        <img src="{{ asset('images/logo-sm-new.png') }}" alt="" height="22">
                     </span>
                     <span class="logo-lg">
-                        <img src="{{ asset('images/logo-light.png') }}" alt="" height="20">
+                        <img src="{{ asset('images/logo-sm-new.png') }}" alt="" height="20">
                     </span>
                 </a>
             </div>
