b112ca2a5e72d6766830895ade0d4cdc51dee43e
thoyiburrohman <thoyibur@gmail.com>
2024-07-26 03:26:07 +0700

    update tele notif bot

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Models\Message;
 use App\Models\Project;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Telegram\Bot\Api;
 
 Route::get('/', function () {
     $data = [
@@ -13,7 +14,20 @@ Route::get('/', function () {
     ];
     return view('home', $data);
 });
-Route::post('/form-message', function (Request $request) {
+Route::post('/form-message', function (Request $request, Api $tele) {
     Message::create($request->all());
+    $dataMessage = Message::where('message', $request->message)->first();
+    $message = 'Assalamualaikum bro, ada pesan dari' .
+        PHP_EOL . PHP_EOL . 'Nama : *' . $dataMessage->name . '*' .
+        PHP_EOL . 'Emailnya : _' . $dataMessage->email . '_' .
+        PHP_EOL . 'Pesannya : *' . $dataMessage->message . '*' .
+        PHP_EOL . 'Web : *Portofolio*' .
+        PHP_EOL . PHP_EOL . 'Terimakasih bro.';
+    $parse = 'Markdown';
+    $tele->sendMessage([
+        'chat_id' => '181194724',
+        'parse_mode' =>  $parse,
+        'text' =>  $message,
+    ]);
     return response()->json(['success' => 'message berhasil terkirim']);
 })->name('form-message');
